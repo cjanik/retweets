cb1535c5311f44d4da7f1ecd3e4bc341a1a465e3
Chris Janik <cjanik@umich.edu>
2015-01-29 18:02:47 -0800

    implementing client subscription

--- client/retweets.js
+++ client/retweets.js
@@ -35,6 +35,16 @@ Template.body.events({
 	}
 });
 
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
+
 var min = 10000,
 	minIndex = 0,
 	numBars = 15;
@@ -51,7 +61,7 @@ function removeMin(numToRemove){
 	return true;
 };
 
-twitterStream.on('tweet', function(id, tweetID, text, rt) {
+twitterStream.on(this.subscriptionId, function(id, tweetID, text, rt) {
 		
 	if(Retweets.find().count() < numBars){
 		var isDuplicate = Retweets.findOne({tweetID: tweetID});
--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ var Twit = new TwitterStreamChannels({
 });
 
 var stream = null,
-  twitterStream = new Meteor.Stream('twitterStream');
+  twitterStream = new Meteor.Stream('twitterStream'),
+  channels = {},
+  lastUpdated = Date.now(),
+  rateLimit = false;
   
 twitterStream.permissions.write(function(eventName) {
   return false;
@@ -21,20 +24,63 @@ twitterStream.permissions.read(function(eventName) {
   return true;
 });
 
+twitterStream.on('client', function(track){
+
+  var self = this;
+
+  addToTrack(self.subscriptionId, track);
+
+  stream.on('channels/'+self.subscriptionId, function(twt){
+      if(twt.retweeted_status){
+        //console.log(twt);
+        twitterStream.emit(self.subscriptionId, twt.id_str, twt.retweeted_status.id_str, twt.text, twt.retweeted_status.retweet_count);
+      }
+    });
+
+});
+
+function addToTrack(subscriber, track){
+  channels[subscriber] = track;
+  updateTwit();
+}
+
+function updateTwit(){
+  var now = Date.now(),
+    updated = false;
+
+  if(now - lastUpdated > 5 && rateLimit === false){
+    Meteor.call('startStream');
+    lastUpdated = now;
+    updated = true;
+  } else if(rateLimit === true && now - lastUpdated > 60){
+    Meteor.call('startStream');
+    lastUpdated = now;
+    rateLimit = false;
+    updated = true;
+  }
+
+  if(!updated){
+    Meteor.setTimeout(updateTwit, 5);
+  }
+
+}
+
 Meteor.methods({
   
-  startStream: function(track, lang){
-    
-    var channels = {
-      'terms': ['california', 'hebdo']
-    }
+  startStream: function(){
   
     stream = Twit.streamChannels({track: channels, language: lang} );
   
-    stream.on('channels/terms', function(twt){
+    stream.on('channels/', function(twt){
       if(twt.retweeted_status){
         //console.log(twt);
         twitterStream.emit('tweet', twt.id_str, twt.retweeted_status.id_str, twt.text, twt.retweeted_status.retweet_count);
+      }
+    });
+
+    stream.on('error', function(data){
+      if(data.statusCode > 200){
+        rateLimit = true;
       }
     });
   },
